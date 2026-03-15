ca3d5587c4fb852b72c157fd4ecb25171e448f0c
SimAbild <abildtrup123@gmail.com>
2026-03-15 16:52:35 +0100

    Post refaktoring

--- employeeJS/config.js
+++ employeeJS/config.js
@@ -1,2 +1,2 @@
 /** Central API-konfiguration for medarbejderportalen. Skift denne ene værdi for at pege på et andet miljø. */
-export const apiBaseUrl = "http://localhost:8080";
+export const apiBaseUrl = "http://4.235.123.111:8080";
--- indexJS/config.js
+++ indexJS/config.js
@@ -1,2 +1,2 @@
 /** Central API-konfiguration for den offentlige side. Skift denne ene værdi for at pege på et andet miljø. */
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "http://4.235.123.111:8080";
